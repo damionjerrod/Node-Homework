4a34c9ead76e42547db789dcd4c6cb3d1df082b4
damionjerrod <damion.dawson@gmail.com>
2019-10-09 15:25:27 -0400

    read event emitter

---
+++ input exercise/readfile.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var emitter = require('events').EventEmitter;
+
+//Function for creating a new document.
+function createFile() {
+    var e = new emitter();
+
+    fs.readFile('newdocument.txt', function (err) {
+        if (err) throw err;
+        e.emit('theProcess');
+    });
+
+    return e;
+}
+
+//Here's the emitter!
+var createIt = createFile();
+
+createIt.on('theProcess', function() {
+    console.log("File Has Been Read!");
+});
