d6c5972794a0121e746e5a29f2e65b10aece3546
damionjerrod <damion.dawson@gmail.com>
2019-10-09 15:26:05 -0400

    write event emitter

---
+++ input exercise/writefile.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var emitter = require('events').EventEmitter;
+
+//Function for creating a new document.
+function createFile() {
+    var e = new emitter();
+
+    fs.writeFile('newdocument.txt', 'Hey There!', function (err) {
+        if (err) throw err;
+        e.emit('theProcess');
+    });
+
+    return e;
+}
+
+//Here's the emitter!
+var createIt = createFile();
+
+createIt.on('theProcess', function() {
+    console.log("Content Written to File!");
+});
